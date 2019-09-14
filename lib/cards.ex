abcefd0733d0f3daf49dd44656ccc3459faa649b
John Joseph <atiquajohn@gmail.com>
2019-09-14 20:26:20 +0700

    write basic function application

--- lib/cards.ex
+++ lib/cards.ex
@@ -12,7 +12,25 @@ defmodule Cards do
       :world
 
   """
-  def hello do
-    'hi there'
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hears", "Dimonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def suffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
