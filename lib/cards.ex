484e4beea0079e58c08cc99d39c245aea3ec6954
John Joseph <atiquajohn@gmail.com>
2019-09-15 17:47:40 +0700

    write logic for error handling

--- lib/cards.ex
+++ lib/cards.ex
@@ -38,4 +38,11 @@ defmodule Cards do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "The file does not exist"
+    end
+  end
 end
