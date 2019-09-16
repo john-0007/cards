0b02a70f170c1997c33809bc5c6fd3f078df94a1
John Joseph <atiquajohn@gmail.com>
2019-09-16 20:27:14 +0700

    wrote the fucntion docs

--- lib/cards.ex
+++ lib/cards.ex
@@ -4,15 +4,8 @@ defmodule Cards do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
+    returning a list of string representing a deck of playing cards
   """
-
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hears", "Dimonds"]
@@ -34,6 +27,18 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divided a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in
+    the hand.
+  ## Example
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
